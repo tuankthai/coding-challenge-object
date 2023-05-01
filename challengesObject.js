89cca0ce974335fc81c22d74a975b144189781af
dsl2022 <2015rpro@gmail.com>
2023-04-30 23:15:37 -0400

    first commit

---
+++ challengesObject.js
@@ -0,0 +1,43 @@
+// Easy
+// 1. Create an array of your favorite movies, and log the second movie in the array to the console.
+const favoriteMovies = [];
+
+// 2. Create an object that represents your favorite animal, with properties for name, species, and age. Log the object to the console.
+const favoriteAnimal = {};
+
+// 3. Create a function that takes an array of numbers as an argument, and returns the sum of all the numbers in the array.
+function sumArray(numbers) {
+  // Add your code here
+}
+
+// Medium
+// 4. Create an object that represents a car, with properties for make, model, and year. Add a method to the object that returns a formatted string with the car's make, model, and year.
+const car = {};
+
+// 5. Create a function that takes an array of objects representing books, with properties for title and author, and returns a new array of objects with properties for title, author, and a boolean value indicating whether the book has been read or not.
+function markBooksRead(books) {
+  // Add your code here
+}
+
+// 6. Create a function that takes an object representing a person, with properties for name, age, and gender, and returns a formatted string with the person's name, age, and pronouns (he/she/they).
+function formatPerson(person) {
+  // Add your code here
+}
+
+// Difficult
+// 7. Create an object that represents a bank account, with properties for account number, balance, and a transaction history (an array of objects representing each transaction, with properties for date, amount, and type). Add methods to the object for depositing and withdrawing money, and for getting the account balance and transaction history.
+const bankAccount = {};
+
+// 8. Create a function that takes an array of objects representing students, with properties for name, grade level, and test scores. The function should return an object with properties for average test score, highest test score, and lowest test score.
+function analyzeTestScores(students) {
+  // Add your code here
+}
+
+// 9. Create a function that takes an array of objects representing employees, with properties for name, job title, and salary. The function should return a new array of objects with properties for name, job title, salary, and a boolean value indicating whether the employee's salary is above or below the average salary.
+function analyzeEmployeeSalary(employees) {
+  // Add your code here
+}
+
+// 10. Create an object that represents a weather forecast, with properties for date, temperature, humidity, and precipitation. Add methods to the object for getting the current weather conditions, predicting the weather for the next day based on historical data, and generating a formatted report with the weather forecast for the next week.
+const weatherForecast = {};
+
